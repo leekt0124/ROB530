33219981d4b5a14a2595cef033015923580be00e
NelsonLee0124 <a06w.68577818@gmail.com>
2022-01-29 11:26:23 -0500

    Working on hw3

--- HW3/leekt_hw3.py
+++ HW3/leekt_hw3.py
@@ -13,8 +13,50 @@ t = mat["t"]
 z_1 = mat["z_1"]
 z_2 = mat["z_2"]
 
+# def H_1(Kf_1, p):
+#     px = p[0][0]
+#     py = p[1][0]
+#     pz = p[2][0]
+#     temp = np.array([[1/pz, 0, -px / pz ** 2], [0, 1/pz, -py / pz ** 2]])
+#     return Kf_1 @ temp
+
+# def H_2(Kf_2, p, R):
+#     px = p[0][0]
+#     py = p[1][0]
+#     pz = p[2][0]
+#     temp = np.array([[1/pz, 0, -px / pz ** 2], [0, 1/pz, -py / pz ** 2]])
+#     return Kf_2 @ temp @ R.T
+
+# def h1(Kf_1, p, C_1):
+#     return Kf_1 @ 
+
 class EKF:
-    def __init__(self):
+    def __init__(self, system, init):
+        self.x = init.x  # state vector
+        self.Sigma = init.Sigma  # state covariance
+
+    def projection(self, p):
+        x = p[0][0]
+        y = p[1][0]
+        z = p[2][0]
+        return np.array([[x / z], [y / z]])
+
+    def H_1(self, Kf_1, p):
+        px = p[0][0]
+        py = p[1][0]
+        pz = p[2][0]
+        temp = np.array([[1/pz, 0, -px / pz ** 2], [0, 1/pz, -py / pz ** 2]])
+        return Kf_1 @ temp
+
+    def H_2(self, Kf_2, p, R):
+        px = p[0][0]
+        py = p[1][0]
+        pz = p[2][0]
+        temp = np.array([[1/pz, 0, -px / pz ** 2], [0, 1/pz, -py / pz ** 2]])
+        return Kf_2 @ temp @ R.T
+
+    def h1(self, Kf_1, p, C_1):
+        return Kf_1 @ 
 
     def predition(self):
 
---
+++ HW3/test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+temp = np.ones((3, 1))
+print(temp[2][0])
